e35cedaeef3648b763560b72c26c3c451e732e00
George <bulakh.96@gmail.com>
2023-05-10 22:32:24 +0300

    Avoid reimplementing parent methods

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
-import { Kysely, DialectAdapter, PostgresDialect, MigrationLockOptions, KyselyPlugin, QueryCompiler, PostgresQueryCompiler, SchemaModule, CreateTableBuilder, QueryExecutor, ColumnBuilderCallback, ColumnDefinitionBuilder, ColumnDefinitionNode, GeneratedNode, OperationNode, ColumnNode, CreateTableBuilderProps } from "kysely"
+import { Kysely, DialectAdapter, PostgresDialect, MigrationLockOptions, KyselyPlugin, QueryCompiler, PostgresQueryCompiler, SchemaModule, CreateTableBuilder, QueryExecutor, ColumnBuilderCallback, ColumnDefinitionBuilder, ColumnDefinitionNode, GeneratedNode, OperationNode, ColumnNode, CreateTableBuilderProps, Expression, OnModifyForeignAction, ColumnDataType, Transaction, TransactionBuilder } from "kysely"
 
-import { createQueryId } from 'kysely/dist/cjs/util/query-id'
-import { CreateTableNode } from 'kysely/dist/cjs/operation-node/create-table-node'
-import { parseTable } from 'kysely/dist/cjs/parser/table-parser'
-import { DataTypeExpression, parseDataTypeExpression } from "kysely/dist/cjs/parser/data-type-parser"
-import { noop, freeze } from 'kysely/dist/cjs/util/object-utils'
+// import { createQueryId } from 'kysely/dist/cjs/util/query-id'
+// import { CreateTableNode } from 'kysely/dist/cjs/operation-node/create-table-node'
+// import { parseTable } from 'kysely/dist/cjs/parser/table-parser'
+// import { DataTypeExpression, parseDataTypeExpression } from "kysely/dist/cjs/parser/data-type-parser"
+// import { noop, freeze } from 'kysely/dist/cjs/util/object-utils'
+// import { DefaultValueExpression } from "kysely/dist/cjs/parser/default-value-parser"
+
+type Arg1<T> = T extends (arg1: infer U, ...args: any[]) => any ? U : never;
+type Arg2<T> = T extends (arg1: any, arg2: infer U, ...args: any[]) => any ? U : never;
 
 class CockroachAdapter implements DialectAdapter {
    #schemaPlugin!: KyselyPlugin
@@ -49,96 +53,211 @@ export class CockroachQueryCompiler extends PostgresQueryCompiler {
    visitGenerated(node: CockroachGeneratedNode) {
       this.append('generated ');
       if (node.always) {
-          this.append('always ');
+         this.append('always ');
       }
       if (node.byDefault) {
-          this.append('by default ');
+         this.append('by default ');
       }
       this.append('as ');
       if (node.identity) {
-          this.append('identity');
+         this.append('identity');
       }
       if (node.expression) {
-          this.append('(');
-          this.visitNode(node.expression);
-          this.append(')');
+         this.append('(');
+         this.visitNode(node.expression);
+         this.append(')');
       }
       if (node.stored) {
-          this.append(' stored');
+         this.append(' stored');
       }
       if (node.virtual) {
-          this.append(' virtual');
+         this.append(' virtual');
       }
    }
+}
+
+function getCallerName() {
+   // Get stack array
+   // const orig = Error.prepareStackTrace;
+   // Error.prepareStackTrace = (error, stack) => stack;
+   // const { stack } = new Error();
+   // Error.prepareStackTrace = orig;
+   
+   // const caller = stack[2];
+   // return caller ? caller.getFunctionName() : undefined;
+   return '><'
 }
 
 export class KyselyCockroach<DB> extends Kysely<DB> {
-   get schema() {
-      return new CockroachSchemaModule(this.getExecutor())
+   get schema(): CockroachSchemaModule {
+      console.log('get schema')
+      return Object.setPrototypeOf(super.schema, CockroachSchemaModule.prototype)
+   }
+   withPlugin(x: KyselyPlugin): KyselyCockroach<DB> {
+      console.log('withPlugin', getCallerName())
+      return Object.setPrototypeOf(super.withPlugin(x), KyselyCockroach.prototype)
+   }
+   withoutPlugins(): KyselyCockroach<DB> {
+      console.log('withoutPlugins', getCallerName())
+      return Object.setPrototypeOf(super.withoutPlugins(), KyselyCockroach.prototype)
+   }
+   withTables<T extends Record<string, Record<string, any>>>(): KyselyCockroach<DB & T> {
+      console.log('withTables')
+      return Object.setPrototypeOf(super.withTables(), KyselyCockroach.prototype);
+   }
+   transaction(): CockroachTransactionBuilder<DB> {
+      return Object.setPrototypeOf(super.transaction(), CockroachTransactionBuilder.prototype) 
+      // const txBuilder = super.transaction()
+      // return {
+      //    setIsolationLevel: txBuilder.setIsolationLevel.bind(txBuilder),
+      //    execute(cb) { 
+      //       return txBuilder.execute(tx => {
+      //          console.log('in tx execute', tx)
+      //          Object.setPrototypeOf(tx, CockrachTransaction.prototype)
+      //          console.log('callback tx execute', tx)
+      //          return cb(tx)
+      //       })
+      //    }
+      // }
    }
 }
 
-class CockroachSchemaModule extends SchemaModule {
-   #executor!: QueryExecutor
-   constructor(executor: QueryExecutor) {
-      super(executor)
-      this.#executor = executor
-   }
-   createTable<TB extends string>(table: TB): CockroachCreateTableBuilder<TB, never> {
-      return new CockroachCreateTableBuilder({
-         queryId: createQueryId(),
-         executor: this.#executor,
-         node: CreateTableNode.create(parseTable(table)),
+// ====
+
+class CockroachTransactionBuilder<DB> extends TransactionBuilder<DB> {
+   execute<T>(callback: (trx: Transaction<DB>) => Promise<T>): Promise<T> {
+      return super.execute(tx => {
+         console.log('in tx execute', tx)
+         return callback(Object.setPrototypeOf(tx, CockrachTransaction.prototype))
       })
    }
 }
 
-class CockroachCreateTableBuilder<TB extends string, C extends string = never> extends CreateTableBuilder<TB, C> {
-   #props: CreateTableBuilderProps
-   constructor(props: CreateTableBuilderProps) {
-      super(props)
-      this.#props = freeze(props)
+// ====
+
+class CockrachTransaction<DB> extends Transaction<DB> {
+   get schema(): CockroachSchemaModule {
+      console.log('tx get schema')
+      // return new CockroachSchemaModule(this.getExecutor());
+      return Object.setPrototypeOf(super.schema, CockroachSchemaModule.prototype)
    }
-   addColumn<CN extends string>(columnName: CN, dataType: DataTypeExpression, build: ColumnBuilderCallback = noop<ColumnDefinitionBuilder>): CreateTableBuilder<TB, C | CN> {
-      const columnBuilder = build(new CockroachColumnDefinitionBuilder(CockroachColumnDefinitionNode.create(columnName, parseDataTypeExpression(dataType))));
-      return new CreateTableBuilder({
-         ...this.#props,
-         node: CreateTableNode.cloneWithColumn(this.#props.node, columnBuilder.toOperationNode()),
-      });
+
+   withPlugin(plugin: KyselyPlugin): CockrachTransaction<DB> {
+      console.log('tx withPlugin')
+      return Object.setPrototypeOf(super.withPlugin(plugin), CockrachTransaction.prototype)
+   }
+   withoutPlugins(): CockrachTransaction<DB> {
+      console.log('tx withoutPlugins')
+      return Object.setPrototypeOf(super.withoutPlugins(), CockrachTransaction.prototype)
+   }
+   withTables<T extends Record<string, Record<string, any>>>(): CockrachTransaction<DB & T> {
+      console.log('tx withTables')
+      return Object.setPrototypeOf(super.withTables(), CockrachTransaction.prototype)
    }
 }
 
-class CockroachColumnDefinitionBuilder extends ColumnDefinitionBuilder {
-   #node: CockroachColumnDefinitionNode
-   constructor(node: CockroachColumnDefinitionNode) {
-      super(node)
-      this.#node = node
+// ====
+
+class CockroachSchemaModule extends SchemaModule {
+   // #executor!: QueryExecutor
+   // constructor(executor: QueryExecutor) {
+   //    super(executor)
+   //    this.#executor = executor
+   // }
+
+   createTable<TB extends string>(table: TB): CockroachCreateTableBuilder<TB, never> {
+      // return new CockroachCreateTableBuilder({
+      //    queryId: createQueryId(),
+      //    executor: this.#executor,
+      //    node: CreateTableNode.create(parseTable(table)),
+      // })
+      console.log('createTable')
+      return Object.setPrototypeOf(super.createTable(table), CockroachCreateTableBuilder.prototype)
    }
-   stored(): ColumnDefinitionBuilder {
-      if (!this.#node.generated) {
+
+   withPlugin(plugin: KyselyPlugin): CockroachSchemaModule {
+      console.log('schema withPlugin')
+      return Object.setPrototypeOf(super.withPlugin(plugin), CockroachSchemaModule.prototype)
+   }
+   withoutPlugins(): CockroachSchemaModule {
+      console.log('schema withoutPlugins')
+      return Object.setPrototypeOf(super.withoutPlugins(), CockroachSchemaModule.prototype)
+   }
+   withSchema(scheme: string): CockroachSchemaModule {
+      console.log('schema withSchema')
+      return Object.setPrototypeOf(super.withSchema(scheme), CockroachSchemaModule.prototype)
+   }
+}
+
+class CockroachCreateTableBuilder<TB extends string, C extends string = never> extends CreateTableBuilder<TB, C> {
+   addColumn<CN extends string>(columnName: CN, dataType: Arg2<CreateTableBuilder<TB, C>['addColumn']>, build?: CockroachColumnBuilderCallback): CockroachCreateTableBuilder<TB, C | CN> {
+      console.log('addColumn', build)
+      const shim = build ? (builder: ColumnDefinitionBuilder) => {
+         return build(Object.setPrototypeOf(builder, CockroachColumnDefinitionBuilder.prototype))
+      } : undefined
+      return Object.setPrototypeOf(super.addColumn(columnName, dataType, shim), CockroachCreateTableBuilder.prototype)
+  }
+}
+export declare type CockroachColumnBuilderCallback = (builder: CockroachColumnDefinitionBuilder) => CockroachColumnDefinitionBuilder;
+
+const xassign = (b: ColumnDefinitionBuilder): CockroachColumnDefinitionBuilder => {
+   // Object.defineProperties(b, Object.getOwnPropertyDescriptors(Object.getPrototypeOf(CockroachColumnDefinitionBuilder)))
+   // return b as CockroachColumnDefinitionBuilder
+   // console.log('buildr', b.virtual, CockroachColumnDefinitionBuilder.prototype.virtual)
+   return Object.setPrototypeOf(b, CockroachColumnDefinitionBuilder.prototype)
+   // Object.entries(Object.getOwnPropertyNames(CockroachColumnDefinitionBuilder)).forEach(([k, v]) => {
+   //    Object.assign(b, Object.getOwnPropertyDescriptor() (CockroachColumnDefinitionBuilder)[k])
+   // })
+   // Object.setPrototypeOf
+   // return b as CockroachColumnDefinitionBuilder
+}
+
+class CockroachColumnDefinitionBuilder extends ColumnDefinitionBuilder {
+   autoIncrement()                                              { return xassign(super.autoIncrement()) }
+   primaryKey()                                                 { return xassign(super.primaryKey()) }
+   references(ref: string)                                      { return xassign(super.references(ref)) }
+   onDelete(onDelete: OnModifyForeignAction)                    { return xassign(super.onDelete(onDelete)) }
+   onUpdate(onUpdate: OnModifyForeignAction)                    { return xassign(super.onUpdate(onUpdate)) }
+   unique()                                                     { return xassign(super.unique()) }
+   notNull()                                                    { return xassign(super.notNull()) }
+   unsigned()                                                   { return xassign(super.unsigned()) }
+   defaultTo(value: Arg1<ColumnDefinitionBuilder['defaultTo']>) { return xassign(super.defaultTo(value)) }
+   check(expression: Expression<any>)                           { return xassign(super.check(expression)) }
+   generatedAlwaysAs(expression: Expression<any>)               { return xassign(super.generatedAlwaysAs(expression)) }
+   generatedAlwaysAsIdentity()                                  { return xassign(super.generatedAlwaysAsIdentity()) }
+   generatedByDefaultAsIdentity()                               { return xassign(super.generatedByDefaultAsIdentity()) }
+   modifyFront(modifier: Expression<any>)                       { return xassign(super.modifyFront(modifier)) }
+   modifyEnd(modifier: Expression<any>)                         { return xassign(super.modifyEnd(modifier)) }
+
+   stored() {
+      if (!this.toOperationNode().generated) {
          throw new Error('stored() can only be called after generatedAlwaysAs');
       }
-      if (this.#node.generated.virtual) {
+      if (this.toOperationNode().generated!.virtual) {
          throw new Error('Column cannot be marked as stored if it\'s marked as virtual');
       }
-      return new ColumnDefinitionBuilder(ColumnDefinitionNode.cloneWith(this.#node, {
-         generated: CockroachGeneratedNode.cloneWith(this.#node.generated, {
+      return new CockroachColumnDefinitionBuilder(ColumnDefinitionNode.cloneWith(this.toOperationNode(), {
+         generated: CockroachGeneratedNode.cloneWith(this.toOperationNode().generated!, {
             stored: true,
          }),
       }))
    }
-   virtual(): ColumnDefinitionBuilder {
-      if (!this.#node.generated) {
+   virtual() {
+      if (!this.toOperationNode().generated) {
          throw new Error('stored() can only be called after generatedAlwaysAs');
       }
-      if (this.#node.generated.stored) {
+      if (this.toOperationNode().generated!.stored) {
          throw new Error('Column cannot be marked as virtual if it\'s marked as stored');
       }
-      return new ColumnDefinitionBuilder(ColumnDefinitionNode.cloneWith(this.#node, {
-         generated: CockroachGeneratedNode.cloneWith(this.#node.generated, {
+      return new CockroachColumnDefinitionBuilder(ColumnDefinitionNode.cloneWith(this.toOperationNode(), {
+         generated: CockroachGeneratedNode.cloneWith(this.toOperationNode().generated!, {
             virtual: true,
          }),
       }))
+   }
+
+   toOperationNode(): CockroachColumnDefinitionNode {
+      return super.toOperationNode();
    }
 }
 
@@ -153,30 +272,31 @@ const CockroachGeneratedNode: Readonly<{
    create(params: CockroachGeneratedNodeParams): GeneratedNode;
    createWithExpression(expression: OperationNode): GeneratedNode;
    cloneWith(node: GeneratedNode, params: CockroachGeneratedNodeParams): GeneratedNode;
-}> = freeze({
-   is(node): node is GeneratedNode {
-      return node.kind === 'GeneratedNode';
-   },
-   create(params) {
-      return freeze({
-         kind: 'GeneratedNode',
-         ...params,
-      });
-   },
-   createWithExpression(expression) {
-      return freeze({
-         kind: 'GeneratedNode',
-         always: true,
-         expression,
-      });
-   },
-   cloneWith(node, params) {
-      return freeze({
-         ...node,
-         ...params,
-      });
-   },
-})
+}> = GeneratedNode
+//    freeze({
+//    is(node): node is GeneratedNode {
+//       return node.kind === 'GeneratedNode';
+//    },
+//    create(params) {
+//       return freeze({
+//          kind: 'GeneratedNode',
+//          ...params,
+//       });
+//    },
+//    createWithExpression(expression) {
+//       return freeze({
+//          kind: 'GeneratedNode',
+//          always: true,
+//          expression,
+//       });
+//    },
+//    cloneWith(node, params) {
+//       return freeze({
+//          ...node,
+//          ...params,
+//       });
+//    },
+// })
 // ==========================
 export declare type ColumnDefinitionNodeProps = Omit<Partial<CockroachColumnDefinitionNode>, 'kind' | 'dataType'>;
 export interface CockroachColumnDefinitionNode extends ColumnDefinitionNode {
@@ -184,45 +304,46 @@ export interface CockroachColumnDefinitionNode extends ColumnDefinitionNode {
    readonly generated?: CockroachGeneratedNode;
 }
 
- const CockroachColumnDefinitionNode: Readonly<{
-    is(node: OperationNode): node is CockroachColumnDefinitionNode;
-    create(column: string, dataType: OperationNode): CockroachColumnDefinitionNode;
-    cloneWithFrontModifier(node: CockroachColumnDefinitionNode, modifier: OperationNode): CockroachColumnDefinitionNode;
-    cloneWithEndModifier(node: CockroachColumnDefinitionNode, modifier: OperationNode): CockroachColumnDefinitionNode;
-    cloneWith(node: CockroachColumnDefinitionNode, props: ColumnDefinitionNodeProps): CockroachColumnDefinitionNode;
-}> = freeze({
-   is(node): node is CockroachColumnDefinitionNode {
-       return node.kind === 'ColumnDefinitionNode';
-   },
-   create(column, dataType) {
-       return freeze({
-           kind: 'ColumnDefinitionNode',
-           column: ColumnNode.create(column),
-           dataType,
-       });
-   },
-   cloneWithFrontModifier(node, modifier) {
-       return freeze({
-           ...node,
-           frontModifiers: node.frontModifiers
-               ? freeze([...node.frontModifiers, modifier])
-               : [modifier],
-       });
-   },
-   cloneWithEndModifier(node, modifier) {
-       return freeze({
-           ...node,
-           endModifiers: node.endModifiers
-               ? freeze([...node.endModifiers, modifier])
-               : [modifier],
-       });
-   },
-   cloneWith(node, props) {
-       return freeze({
-           ...node,
-           ...props,
-       });
-   },
-})
+const CockroachColumnDefinitionNode: Readonly<{
+   is(node: OperationNode): node is CockroachColumnDefinitionNode;
+   create(column: string, dataType: OperationNode): CockroachColumnDefinitionNode;
+   cloneWithFrontModifier(node: CockroachColumnDefinitionNode, modifier: OperationNode): CockroachColumnDefinitionNode;
+   cloneWithEndModifier(node: CockroachColumnDefinitionNode, modifier: OperationNode): CockroachColumnDefinitionNode;
+   cloneWith(node: CockroachColumnDefinitionNode, props: ColumnDefinitionNodeProps): CockroachColumnDefinitionNode;
+}> = ColumnDefinitionNode
+//    freeze({
+//    is(node): node is CockroachColumnDefinitionNode {
+//        return node.kind === 'ColumnDefinitionNode';
+//    },
+//    create(column, dataType) {
+//        return freeze({
+//            kind: 'ColumnDefinitionNode',
+//            column: ColumnNode.create(column),
+//            dataType,
+//        });
+//    },
+//    cloneWithFrontModifier(node, modifier) {
+//        return freeze({
+//            ...node,
+//            frontModifiers: node.frontModifiers
+//                ? freeze([...node.frontModifiers, modifier])
+//                : [modifier],
+//        });
+//    },
+//    cloneWithEndModifier(node, modifier) {
+//        return freeze({
+//            ...node,
+//            endModifiers: node.endModifiers
+//                ? freeze([...node.endModifiers, modifier])
+//                : [modifier],
+//        });
+//    },
+//    cloneWith(node, props) {
+//        return freeze({
+//            ...node,
+//            ...props,
+//        });
+//    },
+// })
 
 // ============================
